5013e1373c655978f1785a80c37a9337dd144d3f
antoinefraboul <antoine.fraboul@protonmail.com>
2020-01-25 14:47:27 +0100

    add lights*

---
+++ Assets/Scripts/LightCollider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCollider : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        Vector3 playerPos = other.transform.position - gameObject.transform.position;
+        Vector3 lightPos = new Vector3(0, -1, 0);
+        float angle = Vector3.Angle(playerPos, lightPos);
+        if (Mathf.Abs(angle) < gameObject.GetComponent<Light>().spotAngle)
+        {
+            int layerMask = 1 << 7 ;
+            layerMask = ~layerMask;
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, playerPos, out hit, Mathf.Infinity, layerMask))
+            {
+                if (hit.transform.gameObject.tag == "Player")
+                  Debug.Log("in light");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("in shadow");
+    }
+}
---
+++ Assets/Scripts/LightManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+
+    public GameObject[] lights;
+    public List<Collider> colliders;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lights = GameObject.FindGameObjectsWithTag("Light");
+
+        foreach(GameObject light in lights)
+        {
+            SphereCollider collider = light.AddComponent<SphereCollider>();
+            light.AddComponent<LightCollider>();
+            collider.radius = light.GetComponent<Light>().range;
+            collider.isTrigger = true;
+            colliders.Add(collider);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
