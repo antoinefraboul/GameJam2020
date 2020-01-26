f1c90f6887e6a2fcf4944aae99b603b4f233eff4
16004249 <remi.lescolan@etudiant.univ-rennes1.fr>
2020-01-26 09:41:03 +0100

    level2

---
+++ Assets/Light_Clipping_Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light_Clipping_Manager : MonoBehaviour
+{
+    public float m_time;
+    private float time;
+    public GameObject light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_time += Time.deltaTime;
+        if (m_time > 5)
+        {
+            m_time = 0;
+            light.SetActive(!(light.activeInHierarchy));
+            Debug.Log("Active in Hierarchy" + gameObject.activeInHierarchy);
+        }
+    }
+}
